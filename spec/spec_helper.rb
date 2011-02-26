c42a77374513b883d4c3e427342d2752cbcdef38
Damian Caruso <damian.caruso@gmail.com>
2011-02-25 21:29:51 -0300

    routes specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,12 +1,28 @@
-ENV["RACK_ENV"] ||= "test"
+ENV["RACK_ENV"] = "test"
+
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 require "pumpkin"
+require "rack/test"
+
+Pumpkin::Application.run! if Pumpkin::Application.run?
 
 # Requires supporting files with custom matchers and macros, etc,
 # in ./support/ and its subdirectories.
 Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each {|f| require f}
 
+module Pumpkin
+  module Test
+    module Methods
+      def app
+        Pumpkin::Application.new
+      end
+    end
+  end
+end
+
 RSpec.configure do |config|
+  config.include Rack::Test::Methods
+  config.include Pumpkin::Test::Methods
   config.mock_with :rspec
 end
