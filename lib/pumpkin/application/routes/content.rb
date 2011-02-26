33d6cee884d3251cba86994a14143e75e67182c1
Damian Caruso <damian.caruso@gmail.com>
2011-02-26 14:50:02 -0300

    added get route to retrive a node

--- lib/pumpkin/application/routes/content.rb
+++ lib/pumpkin/application/routes/content.rb
@@ -1,5 +1,9 @@
 class Pumpkin::Application
-  get "/" do
-    ""
+  get "/*" do
+    content_type :json
+    path = params[:splat].first.split("/")
+    node = Pumpkin::Content.find_by_path(path)
+    raise Sinatra::NotFound if node.nil?
+    node.to_json
   end
 end
--- spec/routes/content_spec.rb
+++ spec/routes/content_spec.rb
@@ -1,8 +1,27 @@
 require 'spec_helper'
 
-describe "GET /" do
-  it "should respond OK" do
-    get '/'
-    last_response.should be_ok
+describe "content" do
+  describe "GET resource" do
+    context "when found" do
+      let(:node) { mock('Pumpkin::Content') }
+
+      before(:each) do        
+        Pumpkin::Content.should_receive(:find_by_path).with(instance_of(Array)).once.and_return(node)
+        node.should_receive(:to_json).and_return({}.to_json)
+        get '/'
+      end
+
+      it { last_response.should be_ok }
+      it { last_response.content_type.should eq("application/json") }
+    end
+    
+    context "when not found" do
+      before(:each) do
+        Pumpkin::Content.should_receive(:find_by_path).with(instance_of(Array)).once.and_return(nil)
+        get '/'
+      end
+
+      it { last_response.not_found?.should be_true }
+    end
   end
 end
